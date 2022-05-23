beefa931642a2078cf152817601dc92838daf873
Samalairbien <74256932+Samalairbien@users.noreply.github.com>
2022-05-23 11:10:48 +0200

    Update config.php

--- pages/config.php
+++ pages/config.php
@@ -8,7 +8,7 @@
          */
     try 
     {
-        $bdd = new PDO("mysql:host=localhost;dbname=satlani;charset=utf8", 'root', '');
+        $bdd = new PDO("mysql:host=172.190.1.52;dbname=satlani;charset=utf8", 'satlani', '1595');
     }
     catch(PDOException $e)
     {
