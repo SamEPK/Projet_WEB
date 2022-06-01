d830bd2c1d99e0f8683cd398fbb136ad3e5df8e7
Samalairbien <74256932+Samalairbien@users.noreply.github.com>
2022-06-01 10:03:12 +0200

    connexion serveur école

--- pages/config.php
+++ pages/config.php
@@ -1,8 +1,8 @@
 <?php 
     try 
     {
-        $bdd = new PDO("mysql:host=localhost;dbname=apweb;charset=utf8", 'root', '');
-        // $bdd = new PDO("mysql:host=172.190.1.52;dbname=satlani;charset=utf8", 'satlani', '1595');
+        // $bdd = new PDO("mysql:host=localhost;dbname=apweb;charset=utf8", 'root', '');
+        $bdd = new PDO("mysql:host=172.190.1.52;dbname=satlani;charset=utf8", 'satlani', '1595');
     }
     catch(PDOException $e)
     {
